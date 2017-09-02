8cb117531e8bd88841c48cef1914cac46a7bc2be
Catherine Thompson <cathythompson0915@gmail.com>
2017-09-01 20:16:14 -0600

    color fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const trackWidth = 3000;
 const headerBlockWidth = 100;
 const trackHeight = 80;
-const cyan300 = '#4DD0E1';
+const cyan900 = '#006064';
 const colors = ['#FFF8E1', '#FFECB3', '#FFE082', '#FFD54F', '#FFCA28', '#FFC107', '#FFB300', '#FFA000',
   '#FF8F00', '#FF6F00', '#FF5722', '#F4511E'];
 
@@ -14,7 +14,7 @@ class App extends Component {
     return (
       <div>
         <div style={{ width: trackWidth, display: 'flex', flexDirection: 'column' }}>
-          <div style={{ background: cyan300, height: 150, display: 'flex' }}>
+          <div style={{ background: cyan900, height: 150, display: 'flex' }}>
             {
               headerArray.map((val, i) => (
                 <div key={i} style={{ borderRight: '1px solid white', height: 150, width: headerBlockWidth }} />
